f11369114bb1c0dc60f991bef862446b3b070a30
Pavel <liakh.pavel@gmail.com>
2023-02-07 20:52:48 +0300

    First commit

---
+++ src/by/teachmeskils/homeworks/MainHW.java
@@ -0,0 +1,12 @@
+package by.teachmeskils.homeworks;
+
+public class MainHW {
+
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+        int c = 6;
+        int sum = a + b + c;
+        System.out.println(sum);
+    }
+}
