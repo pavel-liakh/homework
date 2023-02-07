16ae41e4c4b2a2da65ae09e9ad6dad4f4756faf7
Pavel <liakh.pavel@gmail.com>
2023-02-07 22:24:54 +0300

    inithial commit

--- src/by/teachmeskils/homeworks/MainHW.java
+++ src/by/teachmeskils/homeworks/MainHW.java
@@ -4,9 +4,8 @@ public class MainHW {
 
     public static void main(String[] args) {
         int a = 4;
-        int b = 5;
-        int c = 6;
-        int sum = a + b + c;
+        int b = 8;
+        int sum = a + b;
         System.out.println(sum);
     }
 }
