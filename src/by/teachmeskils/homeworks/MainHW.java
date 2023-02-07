e5f7619f6963ced13c279872c0a60de15c840137
Pavel <liakh.pavel@gmail.com>
2023-02-07 22:35:39 +0300

    change

--- src/by/teachmeskils/homeworks/MainHW.java
+++ src/by/teachmeskils/homeworks/MainHW.java
@@ -5,8 +5,9 @@ public class MainHW {
     public static void main(String[] args) {
         int a = 4;
         int b = 8;
+        int d = 45;
         int c = 34;
-        int sum = a + b + c;
+        int sum = a + b + c + d;
         System.out.println(sum);
     }
 }
